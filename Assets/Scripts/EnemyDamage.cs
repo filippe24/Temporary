11a5eb7290226efe0a4591f9d8cbcfbc24bee2ac
JOVANE Alberto <alberto.jovane@inria.fr>
2019-06-26 19:55:10 +0200

    test for passing from windows to linux

---
+++ Assets/Scripts/EnemyDamage.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDamage : MonoBehaviour
+{
+
+    [SerializeField] Collider collisionMesh;
+    [SerializeField] int hitPoints = 10;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnParticleCollision(GameObject other)
+    {
+        print("I'm hit!");
+        ProcessHit();
+        if (hitPoints <= 0)
+        {
+            KillEnemy();
+        }
+    }
+
+    void ProcessHit()
+    {
+        hitPoints = hitPoints - 1;
+        print("current hitpoints are " + hitPoints);
+    }
+
+    private void KillEnemy()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class EnemyMovement : MonoBehaviour
+{
+    [SerializeField] List<Waypoint> path;
+
+	// Use this for initialization
+	void Start () 
+    {
+        Pathfinder pathfinder = FindObjectOfType<Pathfinder>();
+        var path = pathfinder.GetPath();
+        StartCoroutine(FollowPath(path));
+	}
+
+
+
+     IEnumerator FollowPath(List<Waypoint> path)
+     {
+
+        print("Starting patrol..."); 
+        foreach (Waypoint waypoint in path)
+        {     
+            print(waypoint.name);
+            transform.position = waypoint.transform.position;
+            yield return new WaitForSeconds(2f);
+        }
+        print("Ending patrol");
+    }
+
+    // Update is called once per frame
+    void Update () 
+    {
+
+	}
+}
